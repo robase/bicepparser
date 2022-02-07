813eaf508670610aa5688590e2e1722228c6b798
Rohan <11038379+robase@users.noreply.github.com>
2022-02-07 15:13:28 +1100

    code format

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const fs = require("fs");
 const peggy = require("peggy");
-const { exit } = require("process");
 
 const fileIndex = JSON.parse(
   fs.readFileSync("./examples/index.json", { encoding: "utf-8" })
@@ -11,28 +10,31 @@ const parser = peggy.generate(
 );
 
 const results = [];
-let failures = 0
+let failures = 0;
 fileIndex.forEach((file) => {
-  if (file.filePath.startsWith("000") || true) {
+  const testContent = fs.readFileSync(`./examples/${file.filePath}`, {
+    encoding: "utf-8",
+  });
 
-    const testContent = fs.readFileSync(`./examples/${file.filePath}`, {
-      encoding: "utf-8",
-    });
+  let passed = true;
+  let error;
 
-    let passed = true;
-    let error;
-
-    try {
-      parser.parse(testContent);
-    } catch (e) {
-      passed = false;
-      failures++
-    }
-
-    results.push({ file: file.filePath, passed, error });
-
-    console.log(`| [${file.filePath}](/examples/${file.filePath}) | ${passed ? '🟢 passed' : '🔴 failed' } |`);
+  try {
+    parser.parse(testContent);
+  } catch (e) {
+    passed = false;
+    failures++;
   }
+
+  results.push({ file: file.filePath, passed, error });
+
+  console.log(
+    `| [${file.filePath}](/examples/${file.filePath}) | ${
+      passed ? "🟢 passed" : "🔴 failed"
+    } |`
+  );
 });
 
-console.log(`${results.length - failures}/${results.length} files successfully parsed`);
+console.log(
+  `${results.length - failures}/${results.length} files successfully parsed`
+);
